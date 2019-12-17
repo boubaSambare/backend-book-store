b68069d23231686a0db6def8778616430280cbf5
boubaSambare <boubasambare@yahoo.it>
2019-12-18 00:02:49 +0100

    add comments service

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const booksRouter = require("./src/books")
+const commentsRouter = require("./src/comments")
+const expressListEndpoint = require('express-list-endpoints')
 const cors = require("cors")
 require('dotenv').config()
 
 const server = express();
 
-var whitelist = ['http://localhost:3000', 'http://localhost:3001', 
-                 'http://m5-d7-oct19-fe.herokuapp.com', 'https://m5-d7-oct19-fe.herokuapp.com', process.env.FE_URL]
+var whitelist = ['http://localhost:3000', process.env.FE_URL,'https://bubba-books-store.herokuapp.com','http://localhost:4000']
 var corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
@@ -27,6 +28,7 @@ console.log(process.env.FE_URL)
 server.use(express.json())
 
 server.use("/books", cors(corsOptions), booksRouter)
+server.use("/books",cors(corsOptions), commentsRouter)
 server.get("/test", (req, res) => {
     res.send("working!!!")
 })
@@ -34,3 +36,4 @@ server.get("/test", (req, res) => {
 server.listen(port, () => {
     console.log("I'm listening on port " + port)
 })
+console.log(expressListEndpoint(server))
---
+++ src/comments/index.js
@@ -0,0 +1,71 @@
+const {
+    Router
+} = require("express");
+const {
+    readFile,
+    writeFile
+} = require("fs-extra");
+const uuid = require("uuid/v1")
+const {
+    join
+} = require("path");
+const {
+    check,
+    sanitize,
+    validationResult
+} = require("express-validator");
+
+const commentsPath = join(__dirname, "comments.json");
+const booksPath = join(__dirname, "../books/books.json");
+
+const router = Router();
+
+const getComments = async () => {
+    const buffer = await readFile(commentsPath)
+    return JSON.parse(buffer.toString())
+}
+
+
+const getBooks = async () => {
+    const buffer = await readFile(booksPath)
+    return JSON.parse(buffer.toString())
+}
+
+router.get("/:id/comments", async (req, res) => {
+        const comments = await getComments()
+        const books = await getBooks()
+        const bookIdCheck = books.find(item => item.asin === req.params.id)
+        if (!bookIdCheck)
+            return res.status(400).send('book not found')
+        const filterAllcomments = comments.filter(item => item.bookId === req.params.id)
+        let allComments = [bookIdCheck,{commentList:[...filterAllcomments]}]
+        res.status(200).send(allComments)
+    })
+
+    router.post("/:id/comments",
+    [check('username').exists().withMessage(' username is required'),
+     check('text').isLength({ min:4}).withMessage('your comment must contain at least 10 caractor')]
+     , async (req, res) => {
+         const errors = validationResult(req)
+         if(!errors.isEmpty())
+            res.status(400).send(errors)
+        
+        const comments = await getComments()
+        const books = await getBooks()
+        const bookIdCheck = books.find(item => item.asin === req.params.id)
+        if (!bookIdCheck)
+            return res.status(400).send('book not found')
+        let newComment = {
+            commentId : uuid(),
+            bookId: req.params.id,
+            ...req.body,
+            date: new Date()
+        }
+        console.log("REQUEST IN",req.params.id)
+        comments.push(newComment)
+        await writeFile(commentsPath, JSON.stringify(comments))
+        res.status(200).send('comment created')
+    })
+
+
+module.exports = router
