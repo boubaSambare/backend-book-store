ac12177a61e9d456493915090487c7ef05ba714b
Diego Banovaz <diego@strive.school>
2019-12-17 09:37:06 +0100

    deploy

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require("express")
 const booksRouter = require("./src/books")
+const cors = require("cors")
 
 const server = express();
 
+var whitelist = ['http://localhost:3000', 'http://localhost:3001']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
 const port = process.env.PORT || 4000
 
+//server.use()
 server.use(express.json())
 
-server.use("/books", booksRouter)
+server.use("/books", cors(corsOptions),  booksRouter)
+server.get("/test", (req, res)=>{
+    res.send("working!!!")
+})
 
 server.listen(port, () => {
     console.log("I'm listening on port " + port)
