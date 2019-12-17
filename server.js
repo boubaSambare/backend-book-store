31a1216a745a0ac4b42120015af24787d0ecaa99
Diego Banovaz <diego@strive.school>
2019-12-17 10:36:22 +0100

    use of env

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ require('dotenv').config()
 
 const server = express();
 
-var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://m5-d7-oct19-fe.herokuapp.com', 'https://m5-d7-oct19-fe.herokuapp.com']
+var whitelist = ['http://localhost:3000', 'http://localhost:3001', 
+                 'http://m5-d7-oct19-fe.herokuapp.com', 'https://m5-d7-oct19-fe.herokuapp.com', process.env.FE_URL]
 var corsOptions = {
     origin: function (origin, callback) {
         if (whitelist.indexOf(origin) !== -1) {
@@ -18,6 +19,9 @@ var corsOptions = {
 
 // this allows HEROKU (or whatever else cloud system) to select the PORT that is free for him
 const port = process.env.PORT || 4000
+
+console.log(process.env.USER)
+console.log(process.env.FE_URL)
 
 //server.use()
 server.use(express.json())
