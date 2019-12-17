8451416d55438a3c68552d25d49cb56c21357185
Diego Banovaz <diego@strive.school>
2019-12-17 10:01:26 +0100

    whitelist

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const cors = require("cors")
 
 const server = express();
 
-var whitelist = ['http://localhost:3000', 'http://localhost:3001']
+var whitelist = ['http://localhost:3000', 'http://localhost:3001', 'http://m5-d7-oct19-fe.herokuapp.com', 'https://m5-d7-oct19-fe.herokuapp.com']
 var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
     }
-  }
 }
 
 // this allows HEROKU (or whatever else cloud system) to select the PORT that is free for him
@@ -21,8 +21,8 @@ const port = process.env.PORT || 4000
 //server.use()
 server.use(express.json())
 
-server.use("/books", cors(corsOptions),  booksRouter)
-server.get("/test", (req, res)=>{
+server.use("/books", cors(corsOptions), booksRouter)
+server.get("/test", (req, res) => {
     res.send("working!!!")
 })
 
